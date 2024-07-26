38def0a49432e3e94d85e2bf8615405cb3aedbf6
alba-garcia09 <albagarlaz@gmail.com>
2024-07-26 18:58:55 +0200

    wip

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,42 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import AppRouter from './AppRouter.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  width: 100vw;
+  height: 10vh;
+  background-color: black;
+  color: white;
+`;
+
+const Pages = styled.header`
+  width: 100vw;
+  height: 85vh;
+  background-color: blue;
+  color: black;
+`;
+
+
+const Footer = styled.header`
+  width: 100vw;
+  height: 5vh;
+  background-color: black;
+  color: white;
+`;
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <div>
       <BrowserRouter>
-        <AppRouter />
+        <Header> Estoy en el Header</Header>
+        <Pages> Mi pagina principal
+          {/* <AppRouter /> */}
+        </Pages>
+        <Footer> Estoy en el footer</Footer>
       </BrowserRouter>
     </div>
-    </>
+  </>
 )
