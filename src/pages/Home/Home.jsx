2d38b3cbf232edf7da095c84b455edaac3316c69
alba-garcia09 <albagarlaz@gmail.com>
2024-07-30 19:58:08 +0200

    new changes in css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,7 @@
 function Home(){
   return(
     <>
-    <h1>Este es mi h1
-      <h4>hola</h4>
-      <p>hola prueba</p>
-      <p>hola prueba 1</p>
-    </h1>
-    <h2>Este es mi h2</h2>
-    <h3>Este es mi h3</h3>
-    <p>Este es mi p y quiero un texto un poco mas largo para ver que onda hola hola</p>
-    <button>hola</button>
-    <button>un boton mas grande</button>
-
+    <h1>mi home</h1>
     </>
   )
 }
