603a2fc2d9be458baf4452c23f21ca09a2b2c54c
alba-garcia09 <albagarlaz@gmail.com>
2024-08-26 19:44:30 +0200

    new stiles and estructure with percentages in the pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,288 +1,165 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Logo from '../../assets/whiteLogo.png';
+import { useNavigate } from 'react-router-dom';
 
 const HeaderContainer = styled.header`
+  position: fixed;
+  top: 0;
   width: 100vw;
   height: 10vh;
   background-color: black;
   color: white;
   display: flex;
   align-items: center;
+  justify-content: space-evenly;
   padding: 5px 50px;
 `;
 
-const LogoContainer = styled.div`
+const Column = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
   height: 100%;
+`;
 
-  img {
-    height: 80%;
-    margin: 0 10px;
+const MyLogo = styled.img`
+  height: 100%;
+  width: auto;
+`;
+
+const SearchInput = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  background-color: white;
+  padding: 5px 15px 5px 15px;
+  border-radius: 30px;
+  width: 80%;
+`;
+
+const SuggestionsList = styled.ul`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  background-color: white;
+  border-radius: 10px 10px 10px 10px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  z-index: 1000;
+`;
+
+const SuggestionItem = styled.li`
+  padding: 10px;
+  cursor: pointer;
+  color: black;
+  &:hover {
+    background-color: #f0f0f0;
   }
 `;
 
-const SearchBar = styled.div`
+const SearchIcon = styled.i`
+  margin-left: 15px;
+  font-size: 150%;
+  cursor: pointer;
+`;
+
+const IconColumn = styled.div`
   display: flex;
   align-items: center;
-  width: 100%;
+  justify-content: space-around;
+  height: 100%;
+  gap: 15px;
+`;
+
+const Icon = styled.div`
+  font-size: 180%;
+  cursor: pointer;
+  &:hover {
+    color: var(--secundaryColor);
+  }
 `;
 
 function Header() {
+  const navigate = useNavigate();
+  const [search, setSearch] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const [debounceTimeout, setDebounceTimeout] = useState(null);
+
+  const theProducts = ['jeans', 'nike', 't-shirt', 'skirt', 'cap','shirt', 'bershka', 'mustang'];
+
+  useEffect(() => {
+    if (debounceTimeout) {
+      clearTimeout(debounceTimeout);
+    }
+
+    setDebounceTimeout(
+      setTimeout(() => {
+        if (search) {
+          const filteredSuggestions = theProducts.filter(product =>
+            product.toLowerCase().startsWith(search.toLowerCase())
+          );
+          setSuggestions(filteredSuggestions);
+        } else {
+          setSuggestions([]);
+        }
+      }, 1000)
+    );
+  }, [search]);
+
+  const handleSuggestionClick = (suggestion) => {
+    setSearch(suggestion);
+    setSuggestions([]);
+    navigate(`/filter/${suggestion}`);
+  };
+
   return (
     <HeaderContainer>
-      <div className="container">
-        <div className="row w-100">
-          <div className="col-12 col-md-2">
-            <LogoContainer>
-              <img src={Logo} alt="Logo" />
-            </LogoContainer>
-          </div>
-          <div className="col-12 col-md-5 d-none d-md-flex">
-            <SearchBar>
-              <input type="text" placeholder="Buscar..." className="form-control" />
-            </SearchBar>
-          </div>
-          <div className="col-12 col-md-2 d-none d-md-flex">
-            <LogoContainer>
-              <img src={Logo} alt="Icon 1" />
-              <img src={Logo} alt="Icon 2" />
-            </LogoContainer>
-          </div>
-          <div className="col-12 d-flex d-md-none justify-content-end">
-            <LogoContainer>
-              <img src={Logo} alt="Icon 3" />
-            </LogoContainer>
-          </div>
-        </div>
-      </div>
+      <Column className="col-md-3 col-sm-5">
+        <MyLogo src={Logo} alt="Logo" />
+      </Column>
+
+      <Column className="col-md-6 d-none d-md-flex">
+        <SearchInput>
+          <input
+            type="text"
+            placeholder="What do you want"
+            className="form-control"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+          />
+          <SearchIcon
+            className="bi bi-search"
+            style={{ color: "black" }}
+            onClick={() => navigate(`/filter/${search}`)}
+          />
+          {suggestions.length > 0 && (
+            <SuggestionsList>
+              {suggestions.map((suggestion, index) => (
+                <SuggestionItem key={index} onClick={() => handleSuggestionClick(suggestion)}>
+                  {suggestion}
+                </SuggestionItem>
+              ))}
+            </SuggestionsList>
+          )}
+        </SearchInput>
+      </Column>
+
+      <Column className="d-md-none col-sm-2">
+        <SearchIcon className="bi bi-search" style={{ color: "white" }} />
+      </Column>
+
+      <Column className="col-md-3 col-sm-5">
+        <IconColumn>
+          <Icon className="bi bi-person" onClick={() => navigate('/login')} />
+          <Icon className="bi bi-cart" onClick={() => navigate('/cart')} />
+        </IconColumn>
+      </Column>
     </HeaderContainer>
   );
 }
 
 export default Header;
-
-
-// import React from 'react';
-// import styled from 'styled-components';
-// import Logo from '../../assets/whiteLogo.png';
-
-// const Container = styled.header`
-//   width: 100vw;
-//   height: 10vh;
-//   background-color: black;
-//   color: white;
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-//   padding: 5px 50px;
-// `;
-
-// const LogoContainer = styled.div`
-//   display: flex;
-//   align-items: center;
-//   height:100%;
-
-// `;
-
-// const SearchBar = styled.div`
-//   display: flex;
-//   align-items: center;
-// `;
-
-// function Header() {
-//   return (
-//     <Container className="row">
-//       <LogoContainer className="col-12 col-md-3 col-sm-5">
-//         <img src={Logo} alt="Logo" />
-//       </LogoContainer>
-
-//       <LogoContainer className="col-12 d-md-none col-sm-2">
-//         <img src={Logo} alt="lupa" />
-//       </LogoContainer>
-
-//       <SearchBar className="col-12 col-md-3 d-sm-none">
-//         <input type="text" placeholder="Buscar..." className="form-control" />
-//       </SearchBar>
-//       <LogoContainer className="col-12 col-md-3 col-sm-5">
-//         <img src={Logo} alt="Logo" />
-//         </LogoContainer>
-//     </Container>
-//   );
-// }
-
-// export default Header;
-
-
-// import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import styled from 'styled-components';
-// import Logo from '../../assets/whiteLogo.png';
-// import 'bootstrap-icons/font/bootstrap-icons.css';
-
-// const Container = styled.header`
-//   width: 100vw;
-//   height: 10vh;
-//   background-color: black;
-//   color: white;
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: space-between;
-//   align-items: center;
-//   padding:5px 50px 5px 50px ;
-// `;
-
-// function Header() {
-//   // const navigate = useNavigate();
-//   // const [search, setSearch] = useState('');
-//   return (
-//     <>
-//       <Container>
-//         <div className="col-12 col-lg-2">
-//           <img src={Logo} alt="Logo" style={{ height: '80%' }} />
-//         </div>
-
-//         <div className="col-12 col-lg-8">
-//           <p>aqui el boton de busqueda</p>
-//         </div>
-
-//         <div className="col-12 col-lg-2">
-//         <i class="bi bi-person"></i>
-//         <i class="bi bi-cart"></i>
-//         </div>
-//       </Container>
-
-//     </>
-//   )
-// }
-
-// export default Header;
-
-
-// import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import Logo from '../../assets/whiteLogo.png';
-// // import MyButton from '../ButtonInHeader/MyButton.js';
-// import styled from 'styled-components';
-// import 'bootstrap-icons/font/bootstrap-icons.css';
-
-// const HeaderStyled = styled.header`
-//   width: 100vw;
-//   height: 10vh;
-//   background-color: black;
-//   color: white;
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: space-between;
-//   align-items: center;
-//   padding: 20px 50px;
-//   box-sizing: border-box;
-//   position: relative; /* Add position relative for absolute positioning of the panel */
-// `;
-
-// const ProductNavigationPanel = styled.div`
-//   width: 100vw; /* 100% del ancho de la ventana gráfica */
-//   height: 7.5vh; /* 1% de la altura de la ventana gráfica */
-//   background-color: #0A3E27;
-//   color: white
-//   display: flex;
-//   align-items: center;
-//   padding: 20px 50px; /* Adjust padding */
-//   box-sizing: border-box;
-// `;
-
-// const ButtonContainer = styled.div`
-//   display: flex;
-//   flex-direction: row;
-//   gap: 10px;
-// `;
-
-// const Icon = styled.img`
-//   width: 20px;
-//   height: 20px;
-// `;
-
-// const InputContainer = styled.div`
-//   position: relative;
-//   flex-grow: 1;
-//   margin: 0 20px;
-//   padding: 10px 0;
-// `;
-
-// const InputStyled = styled.input`
-//   width: 100%;
-//   height: 30px;
-//   padding: 5px 30px 5px 10px;
-//   font-size: 16px;
-//   box-sizing: border-box;
-//   border-radius: 15px;
-//   border: 1px solid #ccc;
-// `;
-
-// const SearchIconStyled = styled.img`
-//   position: absolute;
-//   right: 10px;
-//   top: 50%;
-//   transform: translateY(-50%);
-//   width: 20px;
-//   height: 20px;
-//   cursor: pointer;
-// `;
-
-// const ProductItem = styled.li`
-//   margin: 0 10px;
-//   cursor: pointer;
-//   list-style-type: none; /* Remove default list style */
-// `;
-
-// function Header() {
-//   const navigate = useNavigate();
-//   const [search, setSearch] = useState('');
-
-//   return (
-//     <>
-//       <HeaderStyled>
-//         <Logo />
-
-//         <InputContainer>
-//           <InputStyled
-//             placeholder="Buscar..."
-//             onChange={(event) => setSearch(event.target.value)}
-//           />
-//           <SearchIconStyled
-//             src={<i class="bi bi-search"></i>}
-//             alt="Search"
-//             onClick={() => navigate(`/filter/${search}`)}
-//           />
-//         </InputContainer>
-//         <ButtonContainer>
-//           <MyButton>
-//             <Icon
-//               src={<i class="bi bi-person"></i>}
-//               alt="Register"
-//               onClick={() => navigate('/register')}
-//             />
-//           </MyButton>
-//           <MyButton>
-//             <Icon
-//               src={<i class="bi bi-cart"></i>}
-//               alt="Cart"
-//               onClick={() => navigate('/cart')}
-//             />
-//           </MyButton>
-//         </ButtonContainer>
-//       </HeaderStyled>
-
-//       <ProductNavigationPanel>
-//         <ProductItem onClick={() => navigate('/car/filter/shirt')} >Shirt</ProductItem>
-//         <ProductItem onClick={() => navigate('/car/filter/jeans')}>Jeans</ProductItem>
-//         <ProductItem onClick={() => navigate('/car/filter/skirt')}>Skirt</ProductItem>
-//         <ProductItem onClick={() => navigate('/car/filter/socks')}>Socks</ProductItem>
-//         <ProductItem onClick={() => navigate('/car/filter/shoes')}>Shoes</ProductItem>
-//       </ProductNavigationPanel>
-//     </>
-//   );
-// }
-
-// export default Header;
